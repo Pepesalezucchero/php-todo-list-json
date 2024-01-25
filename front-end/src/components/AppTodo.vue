d8d57b2458573962e005cb6aa76c7db54be8b74f
Alessio Rugi <alessiorugi95@gmail.com>
2024-01-25 16:56:31 +0100

    base project created

---
+++ front-end/src/components/AppTodo.vue
@@ -0,0 +1,19 @@
+<script>
+
+export default {
+    name: 'AppTodo',
+    data() {
+        return {
+        
+        }
+    }
+}
+</script>
+
+<template>
+  <h1>Hello World!</h1>
+</template>
+
+<style scoped>
+
+</style>
